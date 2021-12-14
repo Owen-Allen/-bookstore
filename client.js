f4e1ac4484bbbe0d1e8e15358d3f7960a9ce4a55
Lucas Colwell <43223673+Shibbler@users.noreply.github.com>
2021-12-14 18:39:16 -0500

    added search by all

--- client.js
+++ client.js
@@ -67,3 +67,47 @@ function searchByPrice(){
     req.setRequestHeader("Content-Type", "text/html")
 	req.send();
 }
+function searchByAll(){
+    let req = new XMLHttpRequest();
+    //read html data and update page accordingly.
+	req.onreadystatechange = function() {
+		if(this.readyState==4 && this.status==200){
+            console.log(this.response)
+            document.body.innerHTML=this.response;
+        }
+	}
+    //get all data
+    searchConditionTitle = document.getElementById("searchConditionTitle").value
+    if (searchConditionTitle == null){
+        searchConditionTitle = ""
+    }
+    searchConditionAuthor = document.getElementById("searchConditionAuthor").value
+    if (searchConditionAuthor == null){
+        searchConditionAuthor = ""
+    }
+    searchConditionGenre = document.getElementById("searchConditionGenre").value
+    if (searchConditionGenre == null){
+        searchConditionGenre = ""
+    }
+    searchConditionMin = document.getElementById("searchConditionMinPrice").value
+    if (searchConditionMin == null){
+        searchConditionMin = 0
+    }
+    searchConditionMax = document.getElementById("searchConditionMaxPrice").value
+    if (searchConditionMax == null){
+        searchConditionMax = 9999999
+    }
+    
+    let searchCondition={
+        title: searchConditionTitle,
+        author: searchConditionAuthor,
+        genre: searchConditionTitle,
+        min: searchConditionMin,
+        max: searchConditionMax
+    }
+    
+	//Send a get request for new data so we can access the db
+	req.open("POST", `/bookSearchPrice/all`);
+    req.setRequestHeader("Content-Type", "text/html")
+	req.send(JSON.stringify(searchCondition));
+}
